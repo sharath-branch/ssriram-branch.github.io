b3070ae71023dab1a7ae65cee42eb85662056ebe
ssriram-branch <sharath.sriram@branch.io>
2025-05-07 10:24:03 +0530

    sample

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,27 @@ import { useEffect } from 'react'
 
 export default function Home() {
 
+  function handleLogEvent() {
+    const BranchSDK = require('branch-sdk');
+    BranchSDK.logEvent('Custom Button Clicked', {
+      custom_data: 'Clicked from homepage button'
+    });
+    console.log("Branch event logged");
+  }
+
+  function handleLogEvent2() {
+    const BranchSDK = require('branch-sdk');
+    BranchSDK.track('pageview');
+    console.log("Branch event logged");
+  }
+
   useEffect(() => {
     async function initAndFetch() {
       const BranchSDK = (await import('branch-sdk')).default
 
-      BranchSDK.init("key_live_ccQ8piFdCMPVysh8TLmEhghmuCk162Rr")
+      BranchSDK.setRequestMetaData("sample_key", "sample_value")
+
+      BranchSDK.init("key_live_hxaCJGVYN2EoATD8Sw49CaefFveq6fR0")
 
       BranchSDK.data(function (err, data) {
         if (err) {
@@ -43,6 +59,13 @@ export default function Home() {
           priority
         />
       </div>
+      <div>
+          <button onClick={handleLogEvent}>Log Branch Event</button>
+        </div>
+        <div>
+        <button onClick={handleLogEvent2}>Log Branch Event 2</button>
+        </div>
+
     </main>
   )
 }
